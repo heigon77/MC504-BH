0cb573818f5c764e6299a01021cad0e6025827fd
Piethro César <p223549@dac.unicamp.br>
2021-03-29 00:13:02 -0300

    Adiçao da funçao leitura

--- Trabalho1/sudoku.c
+++ Trabalho1/sudoku.c
@@ -12,24 +12,14 @@ void* f_thread(void **v) {
     int **matriz = id[1];
     //int idT = id[0];
     int idT = idThr++; // não consegui usar id[0]
-
-    for (int i = 0; i < 9; i++)
-    {
-        for (int j = 0; j < 9; j++)
-        {
-            if (i == idT)
-            {
+    for (int i = 0; i < 9; i++){
+        for (int j = 0; j < 9; j++){
+            if (i == idT){
                 matriz[i][j] += matriz[i][j] + i;
             }
-            
         }
-        
     }
-
     printf("Essa thread cuida da linha: %d\n", idT);
-    
-
-
     // printf("Endereço da matriz depois da chamada : %d\n", id[1]);
     // printf("conteúdo da matriz tem que ser igual a banana: %d\n",matriz[2][3]);
     //printf("teste leitura matriz %d", );
@@ -37,17 +27,36 @@ void* f_thread(void **v) {
 }
 
 void imprime( int **mat,int l, int c){  //funcao para printar matriz
-
-    for (int i = 0; i < c; i++)
-    {
-        for (int j = 0; j < l; j++)
-        {
+    for (int i = 0; i < c; i++){
+        for (int j = 0; j < l; j++){
             printf("%d ",mat[i][j]);
         }
         printf("\n");
-        
     }
-    
+}
+
+void leitura( int** mat){
+    FILE *fp;
+    char c;
+    int linha=0;
+    int coluna=0;
+    if((fp=fopen("sudoku.txt", "r")) == NULL){
+        printf("Erro na abertura do Arquivo");
+    }else{
+        while( (c=fgetc(fp)) !=EOF) {
+            if(c=='\n'){
+                linha++;
+                coluna=0;
+            }else{
+                if(c!=' '){
+                    mat[linha][coluna] = ((int)c - 48);
+                    coluna++;
+                }
+            }
+        }
+    }
+    imprime(mat,9,9);
+    fclose(fp);
 }
 
 int main(int argc, char const *argv[]){   
@@ -55,63 +64,56 @@ int main(int argc, char const *argv[]){
     int linhas = 9;  //dimensionamos a matriz
 
     int **nums = (int *)malloc((2)*sizeof(int *));//Vetor de Ponteiros
-
     int **matriz = malloc((linhas*sizeof(int *))); //Allocamos a matriz
+
     for(int i=0;i<linhas;i++){
         matriz[i] = malloc((colunas*sizeof(int)));
     }
+
+    leitura(matriz);
+
+    /*
     for(int i=0; i<linhas;i++){//preenchendo ela para testes
         for(int j=0; j<colunas;j++){
             matriz[i][j] = 0;
         }
     }    
 
-    // printf("banana : %d\n", matriz[2][3]);
-
     nums[0] = 1; //Identificador da thread; não consegui usar
     nums[1] = matriz;
-    // printf("maca : %d\n", nums[0]);
-    // printf("endereço de matriz antes de ir pra thread : %d\n", nums[1]);
+
     
 
-    pthread_t thr[linhas];
-      //chamada da thread
+    pthread_t thr[linhas];//chamada das threads
 
-    for (int i = 0; i < linhas; i++) //cria uma thread pra cada coluna
-    {
+    for (int i = 0; i < linhas; i++){ //cria uma thread pra cada coluna
         nums[0] = i; //deveria iniciar cada thread com um identificado, mas como é um ponteiro, ao atualizar muda a thread ja iniciada, aí tinha várias thread com o mesmo identificador
         pthread_create(&thr[i], NULL, f_thread, (void**) nums);
     }
 
 
-    for (int i = 0; i < linhas; i++) // une as threads
-    {
+    for (int i = 0; i < linhas; i++){// une as threads
         pthread_join(thr[i], NULL); 
     }
 
     printf("\nResultado da primeira iteração\n");
-
-    imprime(matriz,linhas,linhas);
+    imprime(matriz,linhas,colunas);
     printf("\n");
 
     idThr = 0; //Reinicia as threads
 
-    for (int i = 0; i < linhas; i++) //cria uma thread pra cada coluna uma segunda vez
-    {
+    for (int i = 0; i < linhas; i++){ //cria uma thread pra cada coluna uma segunda vez
         nums[0] = i;
         pthread_create(&thr[i], NULL, f_thread, (void**) nums);
     }
 
-
-    for (int i = 0; i < linhas; i++) // une as threads
-    {
+    for (int i = 0; i < linhas; i++){ // une as threads
         pthread_join(thr[i], NULL); 
     }
 
-     printf("\nResultado da segunda iteração\n");
-
+    printf("\nResultado da segunda iteração\n");
     imprime(matriz,linhas,colunas);
-
+    */
     free(matriz);
     free(nums);
 
